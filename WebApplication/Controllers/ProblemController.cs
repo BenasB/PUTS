fc73713ece38b03a90bb61a675f55909390606f9
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2019-09-11 01:06:57 +0300

    Added the D in CRUD

--- WebApplication/Controllers/ProblemController.cs
+++ WebApplication/Controllers/ProblemController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 using WebApplication.Models;
 
 namespace WebApplication.Controllers
@@ -13,9 +15,9 @@ namespace WebApplication.Controllers
             dbContext = problemDbContext;
         }
 
-        public IActionResult List()
+        public async Task<IActionResult> List()
         {
-            return View(dbContext.Problems.ToList());
+            return View(await dbContext.Problems.ToListAsync());
         }
 
         public IActionResult Create()
@@ -24,17 +26,47 @@ namespace WebApplication.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Problem model)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind ("Name, Description")]Problem model)
         {
-            if (ModelState.IsValid)
+            try
             {
-                dbContext.Problems.Add(model);
-                dbContext.SaveChanges();
+                if (ModelState.IsValid)
+                {
+                    dbContext.Problems.Add(model);
+                    await dbContext.SaveChangesAsync();
 
-                return RedirectToAction("List");
+                    return RedirectToAction(nameof(List));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes");
             }
 
-            return View();
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Problem problem = await dbContext.Problems.FindAsync(id);
+            if (problem == null)
+            {
+                return RedirectToAction(nameof(List));
+            }
+
+            try
+            {
+                dbContext.Problems.Remove(problem);
+                await dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(List));
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction(nameof(List));
+            }
         }
     }
 }
--- WebApplication/Models/Problem.cs
+++ WebApplication/Models/Problem.cs
@@ -6,6 +6,8 @@
         public string Name { get; set; }
         public string Description { get; set; }
         
+        // string InputDesc
+        // string OutputDesc
         // public List<Test> Tests { get; set; }
     }
 }
