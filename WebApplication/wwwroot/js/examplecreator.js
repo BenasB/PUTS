5e24ff1da32999b2265c2fcb2a54cf156ad88509
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2019-09-23 21:03:54 +0300

    Full CRUD

---
+++ WebApplication/wwwroot/js/examplecreator.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    bindDeleteExample();
+});
+$(document).ajaxComplete(function (event, xhr, settings) {
+    bindDeleteExample();
+});
+
+function bindDeleteExample() {
+    $('a.delete-example').on('click', function () {
+        $(this).parents('div.example-case:first').remove();
+        return false;
+    });
+}
---
+++ WebApplication/wwwroot/js/testcreator.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    bindDeleteTest();
+});
+$(document).ajaxComplete(function (event, xhr, settings) {
+    bindDeleteTest();
+});
+
+function bindDeleteTest() {
+    $('a.delete-test').on('click', function () {
+        $(this).parents('div.test-case:first').remove();
+        return false;
+    });
+}
