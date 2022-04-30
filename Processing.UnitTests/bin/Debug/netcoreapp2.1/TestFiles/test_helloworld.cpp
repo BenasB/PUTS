6e3e873c7bf69551c3e2734af7ba4427bb1f9f78
BenasB <29711974+BenasB@users.noreply.github.com>
2022-04-30 13:43:58 +0300

    Removed build files

--- Processing.UnitTests/bin/Debug/netcoreapp2.1/TestFiles/test_helloworld.cpp
+++
@@ -1,8 +0,0 @@
-// This is a test file and it should only print out the string "Hello World!"
-// Used for Unit Testing
-#include <iostream>
-
-int main()
-{
-	std::cout << "Hello World!" << std::endl;
-}
--- Processing.UnitTests/bin/Debug/netcoreapp2.1/TestFiles/test_timeout.cpp
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-#include <iostream>
-
-int main()
-{
-	Sleep(3000);
-	std::cout << "This message should appear after 3 seconds" << std::endl;
-}
